8b1317e67148f175612c6a7c73316eca3d2de7ac
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-02-03 14:57:27 +0900

    add jQueryUI

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-  $("button:first").click(function () {
-    $(".box").slideUp(1000);
-  });
-  $("button:last").click(function () {
-    $(".box").slideDown(1000);
-  })
+  $("#accordion").accordion();
 });
